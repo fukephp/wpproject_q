3fe1800f68a9a8d6e5ca217f203ec7dc31d382ee
farukh <faruk.hopic@gmail.com>
2023-05-08 12:18:32 +0200

    Format prettier files

--- wp-content/plugins/movies/movies.php
+++ wp-content/plugins/movies/movies.php
@@ -1,46 +1,83 @@
 <?php
+
 /**
  * @package Movies
  */
 /*
 Plugin Name: Movies
-Plugin URI: 
+Plugin URI: movies
 Description: This plugin creates a custom post type for movies.
 Version: 1.0
 Author: Faruk Hopic
 Author URI: 
 */
-function create_movies_post_type() {
-    register_post_type( 'movies',
+function create_movies_post_type()
+{
+    register_post_type(
+        'movies',
         array(
             'labels' => array(
-                'name' => __( 'Movies' ),
-                'singular_name' => __( 'Movie' )
+                'name' => __('Movies'),
+                'singular_name' => __('Movie')
             ),
             'public' => true,
             'has_archive' => true,
-            'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
+            'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
         )
     );
 }
-add_action( 'init', 'create_movies_post_type' );
-function movie_title_callback( $post ) {
-    wp_nonce_field( basename( __FILE__ ), 'movie_title_nonce' );
-    $movie_title = get_post_meta( $post->ID, 'movie_title', true );
-    ?>
+add_action('init', 'create_movies_post_type');
+
+function movie_title_callback($post)
+{
+    wp_nonce_field(basename(__FILE__), 'movie_title_nonce');
+    $movie_title = get_post_meta($post->ID, 'movie_title', true);
+?>
     <p>
-        <label for="movie_title"><?php _e( 'Title', 'textdomain' ); ?></label>
+        <label for="movie_title"><?php _e('Title', 'textdomain'); ?></label>
         <br />
-        <input type="text" name="movie_title" id="movie_title" value="<?php echo esc_attr( $movie_title ); ?>" class="widefat" />
+        <input type="text" name="movie_title" id="movie_title" value="<?php echo esc_attr($movie_title); ?>" class="widefat" />
     </p>
-    <?php
+<?php
 }
 
-function save_movie_meta( $post_id ) {
-    if ( ! isset( $_POST['movie_title_nonce'] ) || ! wp_verify_nonce( $_POST['movie_title_nonce'], basename( __FILE__ ) ) ) {
+function add_movie_meta_box()
+{
+    add_meta_box(
+        'movie_meta_box',
+        __('Movie Details', 'textdomain'),
+        'movie_meta_box_callback',
+        'movies',
+        'normal',
+        'default'
+    );
+}
+
+add_action('add_meta_boxes', 'add_movie_meta_box');
+
+function movie_meta_box_callback($post)
+{
+    wp_nonce_field(basename(__FILE__), 'movie_meta_box_nonce');
+    $movie_title = get_post_meta($post->ID, 'movie_title', true);
+?>
+    <p>
+        <label for="movie_title"><?php _e('Movie Title', 'textdomain'); ?></label>
+        <br />
+        <input type="text" name="movie_title" id="movie_title" value="<?php echo esc_attr($movie_title); ?>" class="widefat" />
+    </p>
+<?php
+}
+
+function save_movie_meta($post_id)
+{
+    if (!isset($_POST['movie_meta_box_nonce']) || !wp_verify_nonce($_POST['movie_meta_box_nonce'], basename(__FILE__))) {
         return $post_id;
     }
-    $movie_title = sanitize_text_field( $_POST['movie_title'] );
-    update_post_meta( $post_id, 'movie_title', $movie_title );
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return $post_id;
+    }
+    $movie_title = sanitize_text_field($_POST['movie_title']);
+    update_post_meta($post_id, 'movie_title', $movie_title);
 }
-add_action( 'save_post_movies', 'save_movie_meta' );
+
+add_action('save_post', 'save_movie_meta');
